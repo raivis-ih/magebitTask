6e4c30be4238617ff1468e0b1f6bcb06299ac6e0
raivis-ih <60087975+raivis-ih@users.noreply.github.com>
2020-05-12 13:32:49 +0300

    Create Bootstrap.php

---
+++ libs/Bootstrap.php
@@ -0,0 +1,61 @@
+<?php
+
+class Bootstrap
+{
+    function __construct()
+    {
+        //Gets url from user input
+
+        $url = isset($_GET['url']) ? $_GET['url'] : null;
+        $url = rtrim($url, '/');
+        $url = explode('/', $url);
+
+        //If nothing is entered after websites homepage redirects back to index page
+
+        if (empty($url[0])) {
+            require 'controllers/index.php';
+            $controller = new index();
+            $controller->index();
+            return false;
+        }
+
+        //Loads controller from inputted url and loads it if the file exists
+
+        $file = 'controllers/' . $url[0] . '.php';
+        if (file_exists($file)) {
+            require $file;
+        } else {
+            $this->error();
+            return false;
+        }
+
+        $controller = new $url[0];
+        $controller->loadModel($url[0]);
+
+        if (isset($url[2])) {
+            if (method_exists($controller, $url[1])) {
+                $controller->{$url[1]}($url[2]);
+            } else {
+                $this->error();
+            }
+        } else {
+            if (isset($url[1])) {
+                if (method_exists($controller, $url[1])) {
+                    $controller->{$url[1]}();
+                } else {
+                    $this->error();
+                }
+            } else {
+                $controller->index();
+            }
+        }
+    }
+
+    function error()
+    {
+        require 'controllers/throwError.php';
+        $controller = new throwError();
+        $controller->index();
+        return false;
+    }
+}
